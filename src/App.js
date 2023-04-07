4f336e408c2f679fce2f8a329b2993d860131a14
Emmanuel HEMERIT <emmanuel@hemer.it>
2023-04-07 23:12:12 +0200

    7/29

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>Pomodoro Clock</h1>
+      <div id="break-label">Break Length</div>
+      <div id="session-label">Session Length</div>
+      <button id="break-decrement">-</button>
+      <button id="session-decrement">-</button>
+      <button id="break-increment">+</button>
+      <button id="session-increment">+</button>
+      <div id="break-length">5</div>
+      <div id="session-length">25</div>
     </div>
   );
 }
